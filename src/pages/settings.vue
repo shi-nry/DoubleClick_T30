94647813d159e578ce04e2781a4ef21b04d168f9
nickrinehart <64943135+nickrinehart@users.noreply.github.com>
2021-12-06 08:36:59 -0800

    initial commit

---
+++ src/pages/settings.vue
@@ -0,0 +1,52 @@
+<template>
+
+<dir>
+<b>
+<h1 style="font-size:3vw">Settings</h1>
+<br/>
+<br/>
+Change Preferences:
+<br/>
+<br/>
+Change Investing Account:
+<br/>
+<br/>
+Sign Out:
+<br/>
+<br/>
+Contact:
+<br/>
+<br/>
+Common Questions:
+<br/>
+<br/>
+Show Support and Share:
+<br/>
+<br/>
+Other, please share:
+</b>
+<br><input  id="help" maxlength="100" name="help" v-model = "help" size="51" type="text" required /><br><br>
+<button type = "button" v-on:click = "getData()">Submit</button>
+
+</dir>
+</template>
+
+    
+<script>
+export default{
+    name: "test" ,
+    data() {
+        return {
+            help: null
+            }
+
+    },
+    methods: { 
+        getData() {
+            console.log("user issue : ", this.help);
+        }
+    }
+
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/pages/home.vue'
 import Test from '/src/pages/test.vue'
 import Login from '/src/pages/login.vue'
-import Trade from '/src/pages/trade.vue'
-
+import Preference from '/src/pages/preference.vue'
+import Settings from '/src/pages/settings.vue'
 
 const routes = [
 
@@ -29,12 +29,18 @@ const routes = [
     },
 
     {
-        path: '/trade',
-        name: 'trade',
-        component: Trade
+        path: '/preference',
+        name: 'preference',
+        component: Preference
+
+    },
+
+    {
+        path: '/settings',
+        name: 'settings',
+        component: Settings
+
     }
-
-
 ]
 
 const router = createRouter({
