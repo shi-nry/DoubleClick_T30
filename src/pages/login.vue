615360c0d5e5ab4fa4dcd752663bf20d6e51da70
colincomstock <95597475+colincomstock@users.noreply.github.com>
2021-12-09 21:49:58 -0800

    Update login.vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,7 +6,7 @@
         <keep-alive>
             <component v-bind:is="component" />
         </keep-alive>
-
+        <!-- Implmented visual assets for login page (Colin) -->
         <div v-if="flag===false">
             <br><img src="../assets/login/dont_have.png" width="486" height="43" color="transparent" alt="Create Account"><br><br>
             <button v-on:click="flag = true; toggle();"><img src="../assets/gradient_buttons/create_act_btn_fixed.png" width="136" height="38" color="transparent" alt="Create Account"></button>
