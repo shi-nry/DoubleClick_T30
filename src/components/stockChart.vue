3b1cd8237ac6eec349b133a56d87b088e98ff8c7
shi-nry <64504261+shi-nry@users.noreply.github.com>
2022-04-11 12:49:42 -0700

    Add files via upload

---
+++ src/components/stockChart.vue
@@ -0,0 +1,36 @@
+<script>
+import { ref } from 'vue'
+import VueTradingView from 'vue-trading-view/src/vue-trading-view.vue';
+
+export default {
+    name: 'stockChart',
+    props: {
+        msg: String
+    },
+    components: {
+        VueTradingView,
+    }
+}
+</script>
+
+<template>
+  <div class="schart">
+      <VueTradingView :options="{
+          width: '650',
+          height: '200',
+          theme: 'light',
+          timezone: 'America/Los_Angeles',
+          hide_top_toolbar: 'true',
+          save_image: 'false',
+          style: '2',
+          hide_legend: 'true',
+      }"/>
+  </div>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
